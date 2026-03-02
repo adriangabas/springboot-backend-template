3261248ecc5bb354dae94990696752caf7aaa819
Adrian Gabas Andreu <adriangabasandreu@gmail.com>
2026-03-02 19:14:41 +0100

    service

---
+++ src/main/java/com/adriangabas/b2b/controller/ProductoController.java
@@ -0,0 +1,40 @@
+package com.adriangabas.b2b.controller;
+
+import com.adriangabas.b2b.model.Producto;
+import com.adriangabas.b2b.service.ProductoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+@RequestMapping("/productos")
+public class ProductoController {
+
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    @GetMapping
+    public List<Producto> obtenerTodos() {
+        return productoService.obtenerTodos();
+    }
+
+    @PostMapping
+    public Producto crear(@RequestBody Producto producto) {
+        return productoService.guardar(producto);
+    }
+
+    @GetMapping("/{id}")
+    public Producto obtenerPorId(@PathVariable Long id) {
+        return productoService.obtenerPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public Producto actualizar(@PathVariable Long id, @RequestBody Producto producto) {
+        return productoService.actualizar(id, producto);
+    }
+
+}
