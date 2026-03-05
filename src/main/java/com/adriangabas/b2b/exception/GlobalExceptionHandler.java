11934855eeddbacf6c4c04765deff5aed71f5879
Adrian Gabas Andreu <adriangabasandreu@gmail.com>
2026-03-05 10:39:22 +0100

    Exception

---
+++ src/main/java/com/adriangabas/b2b/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.adriangabas.b2b.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        List<String> errores = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Validación fallida");
+        response.put("mensajes", errores);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
